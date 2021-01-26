b80ee6556a62496116b0c8fddc2295b45308e391
dvesh3 <divesh.pahuja@pimcore.com>
2021-01-26 19:27:15 +0100

    initial commit

---
+++ src/PimcorePaymentProviderMpay24SeamlessBundle.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Pimcore
+ *
+ * This source file is available under following license:
+ * - Pimcore Enterprise License (PEL)
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     PEL
+ */
+namespace Pimcore\Bundle\EcommerceFrameworkBundle;
+
+
+use Pimcore\Bundle\EcommerceFrameworkBundle\Hobex\Installer;
+use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
+
+class PimcorePaymentProviderMpay24SeamlessBundle extends AbstractPimcoreBundle
+{
+    use PackageVersionTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getComposerPackageName()
+    {
+        return 'pimcore/payment-provider-mpay24-seamless';
+    }
+
+    public function getInstaller()
+    {
+        return $this->container->get(Installer::class);
+    }
+}
