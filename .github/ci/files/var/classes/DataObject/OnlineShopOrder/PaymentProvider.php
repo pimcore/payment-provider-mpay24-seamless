7b2944db282bb7f9bbbf34ba8bdd27d4a737f06f
Ashan Ghimire <ashanghimire10@gmail.com>
2022-12-19 15:20:51 +0545

    Added missing data objects

---
+++ .github/ci/files/var/classes/DataObject/OnlineShopOrder/PaymentProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Pimcore\Model\DataObject\OnlineShopOrder;
+
+use Pimcore\Model\DataObject\Exception\InheritanceParentNotFoundException;
+
+class PaymentProvider extends \Pimcore\Model\DataObject\Objectbrick {
+
+protected $brickGetters = ['PaymentProviderPayPalSmartButton'];
+
+
+protected $PaymentProviderPayPalSmartButton = null;
+
+/**
+* @return \Pimcore\Model\DataObject\Objectbrick\Data\PaymentProviderPayPalSmartButton|null
+*/
+public function getPaymentProviderPayPalSmartButton(bool $includeDeletedBricks = false)
+{
+	if(!$includeDeletedBricks &&
+		isset($this->PaymentProviderPayPalSmartButton) &&
+		$this->PaymentProviderPayPalSmartButton->getDoDelete()) {
+			return null;
+	}
+	return $this->PaymentProviderPayPalSmartButton;
+}
+
+/**
+* @param \Pimcore\Model\DataObject\Objectbrick\Data\PaymentProviderPayPalSmartButton $PaymentProviderPayPalSmartButton
+* @return $this
+*/
+public function setPaymentProviderPayPalSmartButton(\Pimcore\Model\DataObject\Objectbrick\Data\PaymentProviderPayPalSmartButton $PaymentProviderPayPalSmartButton): static
+{
+	$this->PaymentProviderPayPalSmartButton = $PaymentProviderPayPalSmartButton;
+	return $this;
+}
+
+}
+
