82af90f22a2c1ea70f30a975977133c5139b45a0
Ashan Ghimire <ashanghimire10@gmail.com>
2022-12-19 17:56:57 +0545

    Replaced payment provider data

--- .github/ci/files/var/classes/DataObject/OnlineShopOrder/PaymentProvider.php
+++ .github/ci/files/var/classes/DataObject/OnlineShopOrder/PaymentProvider.php
@@ -9,28 +9,28 @@ class PaymentProvider extends \Pimcore\Model\DataObject\Objectbrick {
 protected $brickGetters = ['PaymentProviderMpay24'];
 
 
-protected $PaymentProviderPayPalSmartButton = null;
+protected $PaymentProviderMpay24 = null;
 
 /**
-* @return \Pimcore\Model\DataObject\Objectbrick\Data\PaymentProviderPayPalSmartButton|null
+* @return \Pimcore\Model\DataObject\Objectbrick\Data\PaymentProviderMpay24|null
 */
-public function getPaymentProviderPayPalSmartButton(bool $includeDeletedBricks = false)
+public function getPaymentProviderMpay24(bool $includeDeletedBricks = false)
 {
 	if(!$includeDeletedBricks &&
-		isset($this->PaymentProviderPayPalSmartButton) &&
-		$this->PaymentProviderPayPalSmartButton->getDoDelete()) {
+		isset($this->PaymentProviderMpay24) &&
+		$this->PaymentProviderMpay24->getDoDelete()) {
 			return null;
 	}
-	return $this->PaymentProviderPayPalSmartButton;
+	return $this->PaymentProviderMpay24;
 }
 
 /**
-* @param \Pimcore\Model\DataObject\Objectbrick\Data\PaymentProviderPayPalSmartButton $PaymentProviderPayPalSmartButton
+* @param \Pimcore\Model\DataObject\Objectbrick\Data\PaymentProviderMpay24 $PaymentProviderMpay24
 * @return $this
 */
-public function setPaymentProviderPayPalSmartButton(\Pimcore\Model\DataObject\Objectbrick\Data\PaymentProviderPayPalSmartButton $PaymentProviderPayPalSmartButton): static
+public function setPaymentProviderMpay24(\Pimcore\Model\DataObject\Objectbrick\Data\PaymentProviderMpay24 $PaymentProviderMpay24): static
 {
-	$this->PaymentProviderPayPalSmartButton = $PaymentProviderPayPalSmartButton;
+	$this->PaymentProviderMpay24 = $PaymentProviderMpay24;
 	return $this;
 }
 
