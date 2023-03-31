3560c09d7c5c193ddf88387783603884511fa549
aryaantony92 <arya.antony@pimcore.com>
2023-03-31 09:42:53 +0200

    Fix php stan

--- src/PaymentManager/Payment/Mpay24Seamless.php
+++ src/PaymentManager/Payment/Mpay24Seamless.php
@@ -326,6 +326,9 @@ class Mpay24Seamless extends AbstractPayment implements \Pimcore\Bundle\Ecommerc
         return [];
     }
 
+    /**
+     * @phpstan-ignore-next-line
+     */
     private function addOrderItemPositions(OnlineShopOrder $order, string $paymentType, array $additional): array
     {
         $checkSum = 0.0;
